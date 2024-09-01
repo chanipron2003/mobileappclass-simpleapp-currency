a00a53bc5a49f87910264e84b0e4011bd7738bdb
chanipron2003 <chanipornu65@nu.ac.th>
2024-09-01 23:05:46 +0700

    Add all files

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_currency/screens/lastest_rate.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title : "Exchange Rate",
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Exchange Rate"),
+      ),
+      body:const LatestRate(),
+    ),
+
+  ));
+}
